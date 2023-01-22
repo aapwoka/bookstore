eea0425d877a77d4238253c90b4a25b6d9db3231
aapwoka <65490601+aapwoka@users.noreply.github.com>
2023-01-22 20:37:53 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 var config = {
-  apiKey: "AIzaSyD94SWwZAiJ-wO3EdFyKUd2PxOFRnRS9Jg",
-  authDomain: "patient-47a96.firebaseapp.com",
-  databaseURL: "https://patient-47a96-default-rtdb.firebaseio.com",
-  projectId: "patient-47a96",
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
   storageBucket: "",
-  messagingSenderId: "1080833709367",
-  appId: "1:1080833709367:web:7d0c5ce904f6bc0664f6b2",
-  measurementId: "G-9LSZ9DGFRZ"
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 
