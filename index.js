e03eb6dd77946d07da598fa17a177e1b85faf7e6
aapwoka <65490601+aapwoka@users.noreply.github.com>
2023-01-28 21:01:49 +0300

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,15 @@ var signInBtn = document.getElementById("signIn")
 function signInPage(){
     window.location="signIn.html"
 }
-
+function home(){
+window.location="book_store.html"
+}
+function feedback(){
+window.location="#feedbacks"
+}
+function collection(){
+window.location="collection.html"
+}
 function about(){
   window.location="#about"
 }
