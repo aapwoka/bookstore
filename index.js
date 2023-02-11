b58672f20d56f9445dccf6a990e753fc7657ab4b
aapwoka <65490601+aapwoka@users.noreply.github.com>
2023-02-11 16:21:25 +0300

    Update index.js

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ var config = {
 
 
 firebase.initializeApp(config);
+const dbRef = firebase.database().ref();
+const usersRef = dbRef.child('users');
 var signInBtn = document.getElementById("signIn")
 
 function signInPage(){
