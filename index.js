714eb4be0e355ed7311c6720223fd0fb297b0184
aapwoka <65490601+aapwoka@users.noreply.github.com>
2023-03-13 21:05:52 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,3 @@
-var config = {
-    apiKey: "AIzaSyD94SWwZAiJ-wO3EdFyKUd2PxOFRnRS9Jg",
-    authDomain: "patient-47a96.firebaseapp.com",
-    databaseURL: "https://patient-47a96-default-rtdb.firebaseio.com",
-    projectId: "patient-47a96",
-    storageBucket: "patient-47a96.appspot.com",
-    messagingSenderId: "1080833709367",
-    appId: "1:1080833709367:web:7d0c5ce904f6bc0664f6b2",
-    measurementId: "G-9LSZ9DGFRZ"
-  };
-  
-  
-  
-  firebase.initializeApp(config);
-
 var signInBtn = document.getElementById("signIn")
 //var searchOnline = "Kiyosaki";//document.getElementById("searchOnline").value;
 
