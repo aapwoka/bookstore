ae969825723bd39ea4b3ae1d48083f3e2b0801f8
aapwoka <65490601+aapwoka@users.noreply.github.com>
2023-03-14 18:27:39 +0300

    Update index.js

--- index.js
+++ index.js
@@ -14,38 +14,115 @@ function Search(){
   }
 }
 
-function signUp(){
-  var email = document.getElementById("signUpEmail");
-  var password = document.getElementById("signUpPassword");
-  var password_repeat = document.getElementById("passwordRepeat");
-
-  if(password.value!= password_repeat.value){
-    window.alert("Your passwords do not match")
+function checkUserFullName(){
+  var userSurname = document.getElementById("userFullName").value;
+  var flag = false;
+  if(userSurname === ""){
+      flag = true;
+  }
+  if(flag){
+      document.getElementById("userFullNameError").style.display = "block";
   }else{
-    firebase.auth().createUserWithEmailAndPassword(email, password).then((success) => {
-      var user = firebase.auth().currentUser;
-      var uid;
-      if (user != null) {
-          uid = user.uid;
-      }
-      //var firebaseRef = firebase.database().ref();
-      //firebaseRef.child(uid).set(userData);
-      window.alert("Succesfully Signed Up.Continue to sign in").then((value) => {
-        
-          window.location("signIn.html")
-      });
-  }).catch((error) => {
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      swal({
-          type: 'error',
-          title: 'Error',
-          text: "Error",
-      })
-  });
+      document.getElementById("userFullNameError").style.display = "none";
+  }
 }
+// xxxxxxxxxx User Surname Validation xxxxxxxxxx
+function checkUserSurname(){
+  var userSurname = document.getElementById("userSurname").value;
+  var flag = false;
+  if(userSurname === ""){
+      flag = true;
+  }
+  if(flag){
+      document.getElementById("userSurnameError").style.display = "block";
+  }else{
+      document.getElementById("userSurnameError").style.display = "none";
+  }
+}
+// xxxxxxxxxx Email Validation xxxxxxxxxx
+function checkUserEmail(){
+  var userEmail = document.getElementById("userEmail");
+  var userEmailFormate = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  var flag;
+  if(userEmail.value.match(userEmailFormate)){
+      flag = false;
+  }else{
+      flag = true;
+  }
+  if(flag){
+      document.getElementById("userEmailError").style.display = "block";
+  }else{
+      document.getElementById("userEmailError").style.display = "none";
+  }
+}
+// xxxxxxxxxx Password Validation xxxxxxxxxx
+function checkUserPassword(){
+  var userPassword = document.getElementById("userPassword");
+  var userPasswordFormate = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{10,}/;      
+  var flag;
+  if(userPassword.value.match(userPasswordFormate)){
+      flag = false;
+  }else{
+      flag = true;
+  }    
+  if(flag){
+      document.getElementById("userPasswordError").style.display = "block";
+  }else{
+      document.getElementById("userPasswordError").style.display = "none";
+  }
 }
 
+function signUp(){
+  var userFullName = document.getElementById("userFullName").value;
+  var userSurname = document.getElementById("userSurname").value;
+  var userEmail = document.getElementById("userEmail").value;
+  var userPassword = document.getElementById("userPassword").value;
+  var userFullNameFormate = /^([A-Za-z.\s_-])/;    
+  var userEmailFormate = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  var userPasswordFormate = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{10,}/;      
+
+  var checkUserFullNameValid = userFullName.match(userFullNameFormate);
+  var checkUserEmailValid = userEmail.match(userEmailFormate);
+  var checkUserPasswordValid = userPassword.match(userPasswordFormate);
+
+  if(checkUserFullNameValid == null){
+      return checkUserFullName();
+  }else if(userSurname === ""){
+      return checkUserSurname();
+  }else if(checkUserEmailValid == null){
+      return checkUserEmail();
+  }else if(checkUserPasswordValid == null){
+      return checkUserPassword();
+  }else{
+      firebase.auth().createUserWithEmailAndPassword(userEmail, userPassword).then((success) => {
+          var user = firebase.auth().currentUser;
+          var uid;
+          if (user != null) {
+              uid = user.uid;
+          }
+          var firebaseRef = firebase.database().ref();
+          var userData = {
+              userFullName: userFullName,
+              userSurname: userSurname,
+              userEmail: userEmail,
+              userPassword: userPassword,
+              userFb: "https://www.facebook.com/",
+              userTw: "https://twitter.com/",
+              userGp: "https://plus.google.com/",
+              userBio: "User biography",
+          }
+          firebaseRef.child(uid).set(userData);
+          window.alert("Succesfully Signed Up").then((value) => {
+            
+              
+          });
+      }).catch((error) => {
+          // Handle Errors here.
+          var errorCode = error.code;
+          var errorMessage = error.message;
+      });
+  }
+}
 function SignIn(){
   var email = document.getElementById("signEmail");
   var password = document.getElementById("signPassword");
